a4aa77d5730353bdecc9e176290167927a900f67
JustinMassart <justin.massart@student.hepl.be>
2024-12-10 17:43:58 +0100

    Added some functions to RecipeRepo

---
+++ src/Repository/RecipeRepository.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Recipe;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Recipe>
+ */
+class RecipeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Recipe::class);
+    }
+
+    public function findTotalDuration()
+    {
+        return $this->createQueryBuilder('r')
+            ->select('SUM(r.duration) as total')
+            ->getQuery()
+            ->getSingleResult();
+    }
+
+    /**
+     * @return Recipe[]
+     */
+    public function findWithDurationLowerThan(int $duration): array
+    {
+        return $this->createQueryBuilder('r')
+            ->where('r.duration <= :duration')
+            ->orderBy('r.duration', 'ASC')
+            ->setMaxResults(10)
+            ->setParameter('duration', $duration)
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @return Recipe[]
+     */
+    public function getTenResults(int $page = 1): array
+    {
+        $resultsPerPage = 10;
+        $offset = ($page - 1) * $resultsPerPage;
+
+        return $this->createQueryBuilder('r')
+            ->orderBy('r.createdAt', 'ASC')
+            ->setMaxResults($resultsPerPage)
+            ->setFirstResult($offset)
+            ->getQuery()
+            ->getResult();
+    }
+
+    //    /**
+    //     * @return Recipe[] Returns an array of Recipe objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('r.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Recipe
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
