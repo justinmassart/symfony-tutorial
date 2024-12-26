8282b06ce56d9677851281672cde047bd3bcada5
JustinMassart <justin.massart@student.hepl.be>
2024-12-26 15:41:41 +0100

    Added Factories calls

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\RecipeFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,6 +11,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        UserFactory::createOne([
+            'firstname' => 'Justin',
+            'lastname' => 'Massart',
+            'username' => 'TheDev',
+            'email' => 'justin@mail.com',
+        ]);
+        UserFactory::createMany(25);
         RecipeFactory::createMany(25);
 
         $manager->flush();
